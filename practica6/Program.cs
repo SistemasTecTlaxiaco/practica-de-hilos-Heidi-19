8bc63089de026d7c78827b87d798c5164b664bb9
HEIDI LUCY BAUTISTA SANJUAN <heidi_lbs@tlaxiaco.tecnm.mx>
2022-04-06 16:57:38 -0500

    Hilo con parametros

--- practica6/Program.cs
+++ practica6/Program.cs
@@ -8,28 +8,53 @@ namespace practica6
 {
     class Program
     {
-        static void Main(String[] args)
+        public static int CalculoComplejo(int n)
         {
-            //Crear hilo          expresión blanda
-            Thread t = new Thread(()=>go("Programación con hilos","Es facil!!!"));
-            t.Start();
-            Console.WriteLine("Hilo finalizado");
-            Console.Read();
+            // sumo uno y espero 5 segundos
+            n = n + 1;
+            Thread.Sleep(5000);
+            return n;
         }
 
-        static void go(string str1, string str2)
-        {
-            Console.WriteLine(str1);
-            Console.WriteLine(str2);
-        }
+    }
 
-        static void metodo(object o)
+    public class HiloParaMates
+    {
+        protected int n;
+        protected MatesCallback callback = null;
+        public HiloParaMates(int n, MatesCallback callback)
         {
-            //Imprimir objeto
-            Console.WriteLine(o.ToString());
-            //Escribir mil veces el caracter y
-            for(int i = 0; i < 1000; i++)
-                Console.WriteLine("y");
+            this.n = n;
+            this.callback = callback;
+        }
+        public void CalculoComplejo()
+        {
+            int result = Program.CalculoComplejo(n);
+            if (callback != null)
+                callback(result);
+        }
+    }
+
+    // creo un delegado con la firma necesaria para capturar
+    // el valor devuelto por el método CalculoComplejo
+    public delegate void MatesCallback(int n);
+
+    public class Ejemplo
+    {
+
+        public static void Main()
+        {
+            HiloParaMates hpm = new HiloParaMates(1000, new MatesCallback(ResultCallback));
+
+            Thread th = new Thread(new ThreadStart(hpm.CalculoComplejo));
+
+            th.Start();
+            th.Join();
+
+        }
+        public static void ResultCallback(int n)
+        {
+            Console.WriteLine("Resultado de la operación: " + n);
         }
     }
 }
