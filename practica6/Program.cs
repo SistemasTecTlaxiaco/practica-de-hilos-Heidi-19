01d7428ea0b7627d44bb92872f5f97314fa474b9
HEIDI LUCY BAUTISTA SANJUAN <heidi_lbs@tlaxiaco.tecnm.mx>
2022-04-06 16:07:32 -0500

    crear hilo

--- practica6/Program.cs
+++ practica6/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace practica6
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Crear hilo
+            Thread t = new Thread(metodo);
+
+            //Escribir mil veces el caracter x
+            for(int i = 0; i < 1000; i++)
+                Console.WriteLine("x");
+        }
+
+        static void metodo()
+        {
+            //Escribir mil veces el caracter y
+            for(int i = 0; i < 1000; i++)
+                Console.WriteLine("y");
+        }
+    }
+}
+
+
